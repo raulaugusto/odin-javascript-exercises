a33af522bfe0f5855168b3683e2797ab808cbc14
Raul Santos <contatoraulaugusto@gmail.com>
2025-12-04 09:20:56 -0300

    Solved 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+const palindromes = function (string) {
+  const clean = string
+    .split("")
+    .filter((char) => alphanumerical.includes(char))
+    .join("");
+  const reversed = clean.split("").reverse().join("");
+  return clean === reversed;
 };
+console.log(palindromes("banab"));
+console.log(palindromes("banaba"));
 
 // Do not edit below this line
 module.exports = palindromes;
