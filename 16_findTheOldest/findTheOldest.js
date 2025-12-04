20055705aca5c6e69ca6e7ea20838803072519fb
Raul Santos <contatoraulaugusto@gmail.com>
2025-12-04 10:06:16 -0300

    Solved 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, person) => {
+    const curr = getAge(person.yearOfBirth, person.yearOfDeath);
+    const prev = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    return curr > prev ? person : oldest;
+  });
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
